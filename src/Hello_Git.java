31325d5d196de91fc1af6524217868b83a414c0b
osman <osmannngulerrr3@gmail.com>
2023-07-29 19:29:03 +0300

    version1

---
+++ src/Hello_Git.java
@@ -0,0 +1,11 @@
+public class Hello_Git {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+
+
+
+
+
+    }
+}
---
+++ src/gitStage.java
@@ -0,0 +1,11 @@
+public class gitStage {
+    public static void main(String[] args) {
+
+        System.out.println("hello stage");
+
+
+
+
+
+    }
+}
